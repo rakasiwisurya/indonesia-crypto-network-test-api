df87d5164614ac5858ce76b72d6dc474990077a9
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2025-10-21 10:40:40 +0700

    change config key to process.env

--- src/common/supabase/supabase.service.ts
+++ src/common/supabase/supabase.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 @Injectable()
 export class SupabaseService {
   private readonly supabase: SupabaseClient;
 
-  constructor(private readonly config: ConfigService) {
-    const url = this.config.get<string>("SUPABASE_URL");
-    const anonKey = this.config.get<string>("SUPABASE_ANON_KEY"); //unused for conditional role key
-    const serviceRoleKey = this.config.get<string>("SUPABASE_SERVICE_ROLE_KEY");
+  constructor() {
+    const url = process.env.SUPABASE_URL;
+    const anonKey = process.env.SUPABASE_ANON_KEY; //unused for conditional role key
+    const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
     if (!url || !anonKey || !serviceRoleKey) {
       throw new Error("Missing Supabase configuration values");
