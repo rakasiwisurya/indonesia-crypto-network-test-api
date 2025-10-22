74257a62f3aacc40347bec26f6c8d4ca929886ce
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2025-10-22 22:54:21 +0700

    change SMTP from GMail to Resend

--- src/common/mail/mail.service.ts
+++ src/common/mail/mail.service.ts
@@ -1,41 +1,70 @@
+// import { Injectable } from "@nestjs/common";
+// import * as nodemailer from "nodemailer";
+// import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+// @Injectable()
+// export class MailService {
+//   private transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>;
+
+//   constructor() {
+//     this.transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.GMAIL_USER,
+//         pass: process.env.GMAIL_APP_PASSWORD,
+//       },
+//     });
+//   }
+
+//   async sendMail({
+//     to,
+//     subject,
+//     text,
+//     html,
+//   }: {
+//     to: string;
+//     subject: string;
+//     text?: string;
+//     html?: string;
+//   }) {
+//     const info = await this.transporter.sendMail({
+//       from: `"Indonesia Crypto Network Test" <${process.env.GMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+
+//     console.info("Message sent: %s", info.messageId);
+//     return info;
+//   }
+// }
+
 import { Injectable } from "@nestjs/common";
-import * as nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { Resend } from "resend";
 
 @Injectable()
 export class MailService {
-  private transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>;
+  private resend: Resend;
 
   constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD,
-      },
-    });
+    this.resend = new Resend(process.env.RESEND_API_KEY);
   }
 
-  async sendMail({
-    to,
-    subject,
-    text,
-    html,
-  }: {
-    to: string;
-    subject: string;
-    text?: string;
-    html?: string;
-  }) {
-    const info = await this.transporter.sendMail({
-      from: `"Indonesia Crypto Network Test" <${process.env.GMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html,
-    });
+  async sendMail({ to, subject, html }: { to: string; subject: string; html: string }) {
+    try {
+      const result = await this.resend.emails.send({
+        from: `Indonesia Crypto Network Test <${process.env.GMAIL_USER}>`,
+        to,
+        subject,
+        html,
+      });
 
-    console.info("Message sent: %s", info.messageId);
-    return info;
+      console.info(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
